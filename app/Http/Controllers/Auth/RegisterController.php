26a427b44a4164d16c773007b0292472f430d0cf
Marin Cunup <25markyzzz@gmail.com>
2026-04-11 23:56:32 +0300

    #13 Update RegisterController to return AuthResource instead of JsonResponse

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,20 +1,15 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Http\Resources\AuthResource;
 use App\Models\User;
-use App\Notifications\VerifyEmailNotification;
-use Illuminate\Http\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 
 class RegisterController extends Controller
 {
-    public function __invoke(RegisterRequest $request): JsonResponse
+    public function __invoke(RegisterRequest $request): AuthResource
     {
         $user = User::create($request->validated());
 
