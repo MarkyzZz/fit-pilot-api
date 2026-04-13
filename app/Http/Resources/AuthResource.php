63e89cba6c19821a9f9dcc57f6661bda8db027d3
Marin Cunup <25markyzzz@gmail.com>
2026-04-13 22:51:59 +0300

    #13 Remove `email_verified_at` from `AuthResource` JSON structure

--- app/Http/Resources/AuthResource.php
+++ app/Http/Resources/AuthResource.php
@@ -19,7 +19,6 @@ class AuthResource extends JsonResource
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
             'email' => $this->email,
-            'email_verified_at' => $this->email_verified_at,
         ];
     }
 }
